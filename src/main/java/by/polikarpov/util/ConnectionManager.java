11a0b6dc6f211b8f7d2872ce59e085f2b12f63bf
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-10-15 11:23:18 +0300

    Modification connect with DB

--- src/main/java/by/polikarpov/util/ConnectionManager.java
+++ src/main/java/by/polikarpov/util/ConnectionManager.java
@@ -7,18 +7,34 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionManager {
-    public static Connection getConnection() throws SQLException {
-        Properties properties = new Properties();
+
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+
+    static {
+        loadDriver();
+    }
+
+    private static void loadDriver() {
         try {
-            properties.load(ConnectionManager.class.getClassLoader()
-                    .getResourceAsStream("application.properties"));
-            return DriverManager.getConnection(
-                    properties.getProperty("url"),
-                    properties.getProperty("user"),
-                    properties.getProperty("password")
-            );
-        } catch (IOException e) {
-            throw new RuntimeException("Не загрузились данные БД", e);
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ConnectionManager() {}
 }
