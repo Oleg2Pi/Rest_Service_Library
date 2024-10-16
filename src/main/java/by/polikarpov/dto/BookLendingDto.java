a2b2a36d65445476c11dcf0d1573d8977aaa3f54
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-10-16 09:51:31 +0300

    Write javadoc for BookLendingDto

--- src/main/java/by/polikarpov/dto/BookLendingDto.java
+++ src/main/java/by/polikarpov/dto/BookLendingDto.java
@@ -1,4 +1,11 @@
 package by.polikarpov.dto;
 
+/**
+ * Data Transfer Object (DTO) representing a book lending transaction.
+ * This record associates a reader with a book they are lending.
+ *
+ * @param readers the details of the readerDto involved in the lending
+ * @param books the details of the bookDto being lent
+ */
 public record BookLendingDto(ReadersDto readers, BooksDto books) {
 }
