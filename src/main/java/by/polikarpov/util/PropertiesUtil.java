473b22814f94d875ace02d6330aa100470d746c3
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-10-16 10:23:59 +0300

    Write javadoc for PropertiesUtil

--- src/main/java/by/polikarpov/util/PropertiesUtil.java
+++ src/main/java/by/polikarpov/util/PropertiesUtil.java
@@ -3,13 +3,24 @@ package by.polikarpov.util;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Utility class for loading and accessing properties from a properties file.
+ * This class loads the properties defined in "application.properties"
+ * and provides a method to retrieve property values by key.
+ */
 public class PropertiesUtil {
     private static final Properties PROPERTIES = new Properties();
 
+    // Static block to load properties when the class is loaded
     static {
         loadProperties();
     }
 
+    /**
+     * Loads properties from the "application.properties" file into the
+     * PROPERTIES object. This method is called during the static initialization
+     * of the class.
+     */
     private static void loadProperties() {
         try (var inputStream = PropertiesUtil.class.getClassLoader()
                 .getResourceAsStream("application.properties")) {
@@ -19,7 +30,16 @@ public class PropertiesUtil {
         }
     }
 
+    /**
+     * Retrieves the value associated with the specified key from the loaded properties.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value associated with the specified key, or null if the key does not exist
+     */
     public static String get(String key) { return PROPERTIES.getProperty(key); }
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
     private PropertiesUtil() {}
 }
