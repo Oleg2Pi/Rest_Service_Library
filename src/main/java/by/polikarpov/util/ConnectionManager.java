936bfb6a016427c1162a454c5ea09df7a5097939
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-10-16 10:23:45 +0300

    Write javadoc for ConnectionManager

--- src/main/java/by/polikarpov/util/ConnectionManager.java
+++ src/main/java/by/polikarpov/util/ConnectionManager.java
@@ -1,21 +1,31 @@
 package by.polikarpov.util;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
+/**
+ * Utility class for managing database connections.
+ * This class loads the PostgreSQL driver and provides a method to retrieve
+ * database connections using settings defined in a properties file.
+ */
 public class ConnectionManager {
 
     private static final String URL_KEY = "db.url";
     private static final String USERNAME_KEY = "db.username";
     private static final String PASSWORD_KEY = "db.password";
 
+    // Static block to load the JDBC driver when this class is loaded
     static {
         loadDriver();
     }
 
+    /**
+     * Loads the PostgreSQL JDBC driver.
+     * This method is called during the static initialization of the class.
+     *
+     * @throws RuntimeException if the driver class cannot be found
+     */
     private static void loadDriver() {
         try {
             Class.forName("org.postgresql.Driver");
@@ -24,6 +34,12 @@ public class ConnectionManager {
         }
     }
 
+    /**
+     * Retrieves a connection to the database using the specified properties.
+     *
+     * @return a Connection object to the database
+     * @throws SQLException if a database access error occurs, or the URL is null
+     */
     public static Connection getConnection() throws SQLException {
         try {
             return DriverManager.getConnection(
@@ -36,5 +52,9 @@ public class ConnectionManager {
         }
     }
 
-    private ConnectionManager() {}
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private ConnectionManager() {
+    }
 }
