2fca0fde69fa10d25c47c5a461c095008beaecd6
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-10-16 09:51:41 +0300

    Write javadoc for BooksDto

--- src/main/java/by/polikarpov/dto/BooksDto.java
+++ src/main/java/by/polikarpov/dto/BooksDto.java
@@ -2,5 +2,13 @@ package by.polikarpov.dto;
 
 import by.polikarpov.entity.Library;
 
+/**
+ * Data Transfer Object (DTO) representing a book.
+ *
+ * @param id the unique identifier of the book
+ * @param title the title of the book
+ * @param author the author of the book
+ * @param library the library to which the book belongs
+ */
 public record BooksDto(Long id, String title, String author, Library library) {
 }
